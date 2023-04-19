cfc77f316cb6b74a3356a1db74702bca1777c4ea
Dennis Ocaña <dennisocana@gmail.com>
2023-04-19 16:46:46 -0500

    Create Ejercicios.sql

---
+++ Tarea DML 19-4/Ejercicios.sql
@@ -0,0 +1,35 @@
+----------------------------------------------------------------------------------------
+--Tarea DML
+--Autores:
+--Chasipanta Pablo
+--Ocaña Dennis
+--Ramos Xavier
+--Version 1.0
+--Última actualización: 19/04/2023
+----------------------------------------------------------------------------------------
+
+----------------------------------------------------------------------------------------
+
+--Ejercicio 1
+
+--Indicación:Número de resultados realizados por examen de aquellos que no requieren ayuno.
+--La consulta deberá devolver tres columnas: "Examen", "Días de Resultado" y "Número de Préstamos" 
+
+--Se realiza un select para indicar los datos que queremos extraer
+--Se especifica las columnas que deseamos siendo estas:
+--Examen.Nombre y la nombramos "Examen"
+--Examen.diasResultado y se nombre "Días de Resultado"
+--Resultado.idResultado la cual se hace un conteo de existencias y se nombra "Préstamos realizados"
+SELECT E.nombre AS 'Exámen', diasResultado AS 'Días de Resultado', COUNT(idResultado) AS 'Préstamos realizados' FROM Examen E 
+INNER JOIN Resultado R ON E.idExamen = R.idExamen	--Se realiza un inner join para unir los examenes con los resultados obtenidos y asi poder contarlos
+WHERE E.ayuno = 0 -- Se establece una condición para mostrar solo los resultados de aquellos que no requieran ayuno
+GROUP BY E.nombre, E.diasResultado --los agrupamos por nombre y los dias resultado debido a la operacion COUNT usada
+
+----------------------------------------------------------------------------------------
+
+----------------------------------------------------------------------------------------
+--Ejercicio 2
+--Indicación
+
+
+----------------------------------------------------------------------------------------
