2a5be2a1ef46d8af2ecaf02e4973034d7fea6faa
Dennis Ocaña <dennisocana@gmail.com>
2023-04-19 17:02:29 -0500

    Update Ejercicios.sql

--- Tarea DML 19-4/Ejercicios.sql
+++ Tarea DML 19-4/Ejercicios.sql
@@ -32,12 +32,24 @@ GROUP BY E.nombre, E.diasResultado --los agrupamos por nombre y los dias resulta
 --Indicación: Nombre y apellido de los pacientes (en una sola columna con título "Paciente") 
 --y número de exámenes realizado, de quienes se han realizado 3 o más exámenes.
 
+--Versión uno: cantidad TOTAL de examenes
 --Se realiza un select de la tabla pacientes y se declara una columna "Paciente"
 --la cual contendra el nombre + ' ' + apellido de la persona en una sola celda
--- tambien tendra una columna contador para saber el numero de examenes que se ha realizado
+-- tambien tendra una columna contador para saber el numero de exámenes que se ha realizado
 SELECT (P.nombre+' '+P.apellido) AS 'Paciente', COUNT(idResultado) AS 'Número de exámenes realizados' FROM Paciente P
-INNER JOIN Resultado R ON P.idUsuario = R.idUsuario
-GROUP BY P.nombre+' '+P.apellido
-HAVING COUNT(idResultado) >= 3
-
+INNER JOIN Resultado R ON P.idUsuario = R.idUsuario -- Realizamos un inner join a resultado ya que podemos saber asi el numero de examenes en total que se ha realizado el paciente sin importar si son diferentes o no
+GROUP BY P.nombre+' '+P.apellido --Se excribe la sentencia para agrupar resultados debido a la operacion count que realizamos
+HAVING COUNT(idResultado) >= 3 --Se implementa la condicion para mostrar resultados
+----------------------------------------------------------------------------------------
+--Version 2 Cantidad de TIPOS de examen
+--Se realiza un select de la tabla pacientes y se declara una columna "Paciente"
+--la cual contendra el nombre + ' ' + apellido de la persona en una sola celda
+-- tambien tendra una columna contador para saber el numero de TIPOS de exámenes que se ha realizado
+SELECT (P.nombre+' '+P.apellido) AS 'Paciente', COUNT(idResultado) AS 'Número de exámenes realizados' FROM Paciente P
+INNER JOIN Resultado R ON P.idUsuario = R.idUsuario -- Realizamos un inner join a resultado ya que podemos saber asi el numero de examenes se ha realizado el paciente
+GROUP BY P.nombre+' '+P.apellido, r.idExamen 
+--Se excribe la sentencia para agrupar resultados debido a la operacion count que realizamos, 
+--además permite agrupar por tipo de examen para saber cuantos TIPOS de examen se ha 
+--realizado el paciente
+HAVING COUNT(idResultado) >= 3--Se implementa la condicion para mostrar resultados
 ----------------------------------------------------------------------------------------
